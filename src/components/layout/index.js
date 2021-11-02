61ad97b670df58a86cc00be3014799862abcf6be
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-11-01 21:00:48 -0300

    feat: adiciona layout

---
+++ src/components/layout/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./styled";
+
+const Layout = ({ children }) => {
+  return <S.WrapperLayout>{children}</S.WrapperLayout>;
+};
+
+export default Layout;
